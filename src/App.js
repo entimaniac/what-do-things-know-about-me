8333c91857a43b15a17aaef0155e3e88c543b735
Jesse Strode <jesse_strode@condenast.com>
2020-08-21 15:20:02 -0400

    add ip. add ability to generate uuid (v5) from set of derived data (fingerprinted)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { v5 as uuidv5 } from 'uuid';
+
 import "./App.css";
 
 function App() {
-  const [clipboard, setClipboard] = useState();
-  const [geolocation, setGeolocation] = useState();
-  const [ip, setIp] = useState();
+  const [clipboard, setClipboard] = useState("");
+  const [geolocation, setGeolocation] = useState("");
+  const [ip, setIp] = useState("");
+  const [id, setId] = useState("");
 
   useEffect(() => {
     fetch(`https://api.ipify.org/?format=json`)
@@ -42,6 +45,17 @@ function App() {
     navigator.geolocation.getCurrentPosition(success, error, options);
   };
 
+  const generateId = () => {
+    let data = {
+      ip: ip,
+      cookie: navigator.cookieEnabled,
+    };
+
+      const MY_NAMESPACE = '091be5fd-9eb4-45af-a4d2-8a0b0f6c8369';
+      let id = uuidv5(JSON.stringify(data), MY_NAMESPACE);
+      setId(id)
+  };
+
   return (
     <div className="App">
       <h2>Totally available data</h2>
@@ -63,14 +77,21 @@ function App() {
 
       <h2>Interesting but sorta safe things:</h2>
       <p>
-        clipboard
+        clipboard:
         <button onClick={readClipboard}>Read Clipboard</button>
         {clipboard}
       </p>
       <p>
-        geolocation
+        geolocation:
         <button onClick={getGeolocation}>Read GeoLocation</button>
         {geolocation}
+      </p>
+
+      <h2>Generate ID based on detected data </h2>
+      <p>
+        ID:
+        <button onClick={generateId}>generate</button>
+        {id}
       </p>
     </div>
   );
