edeba74f6de5ff32d05dd378f3375f220145b1e1
Jesse Strode <jesse_strode@condenast.com>
2020-08-21 11:53:00 -0400

    Updates

---
+++ precache-manifest.e3c414fb362a5564bcf6810b75a6dfe9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032e38254d6894493d7abb0690a55c8e",
+    "url": "/what-do-things-know-about-me/index.html"
+  },
+  {
+    "revision": "7be8d69d18bff5eb48c0",
+    "url": "/what-do-things-know-about-me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bd5a66af256ba89c905",
+    "url": "/what-do-things-know-about-me/static/js/2.1e7d27d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/what-do-things-know-about-me/static/js/2.1e7d27d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be8d69d18bff5eb48c0",
+    "url": "/what-do-things-know-about-me/static/js/main.bbc1cc4c.chunk.js"
+  },
+  {
+    "revision": "31fe771ef61e0ccb75b1",
+    "url": "/what-do-things-know-about-me/static/js/runtime-main.3de390b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/what-do-things-know-about-me/static/media/logo.5d5d9eef.svg"
+  }
+]);
